bb98f46aa92b182929cc15d38c254ab4a58784c8
lNektosl <105857065+lNektosl@users.noreply.github.com>
2024-06-11 15:36:32 +0300

    remove trash + fixed buttonsCheck + added sendMail() Check+ centered form on dif resol + added scroll to reg + removed unnecesarry buttons + added link to tellegram

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,37 +1,51 @@
 var popup;
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   popup = document.getElementById("popUp");
-
   emailjs.init("0__CYpWZ5VcRaQyr4");
+
+ await loadVideos('carousel-track1', 'PL0EWiZIxqKsPg0o38WYlOz98RAtXl10vz');
+ await loadVideos('carousel-track2', 'PL0EWiZIxqKsOLB59zXixFXnLXowWT4GC1');
+
+  
+
+  var continers = document.getElementsByClassName("video-limiter");
+  Array.from(continers).forEach(continer => {
+    buttonCheck(continer);
+  })
 });
 
-function sendMail(){
+function sendMail() {
   var name = document.getElementById("name").value;
   var email = document.getElementById("email").value;
   var games = [];
-  
+
   if (document.getElementById("g1").checked) {
-      games.push("Эгоист/Эгоистка");
+    games.push("Эгоист/Эгоистка");
   }
   if (document.getElementById("g2").checked) {
-      games.push("Долина хранительниц женской души");
+    games.push("Долина хранительниц женской души");
   }
   if (document.getElementById("g3").checked) {
-      games.push("NetWorker");
+    games.push("NetWorker");
   }
-  
-  var params = {
+  if (name != "" && email != "" && games != "") {
+    var params = {
       from_name: name,
       email: email,
-      games: games.join(", ") // Преобразуем массив в строку, разделенную запятыми
-  };
-  emailjs.send("service_4rlwjy9","template_rvlmjhh",params).then(function(response) {
-     console.log("Письмо успешно отправлено!", response);
-     alert("Ваше сообщение успешно отправлено!");
- }, function(error) {
-     console.error("Ошибка отправки письма:", error);
-     alert("Произошла ошибка при отправке сообщения. Пожалуйста, попробуйте еще раз.");
- });}
+      games: games.join(", ")
+    };
+    emailjs.send("service_4rlwjy9", "template_rvlmjhh", params).then(function (response) {
+      console.log("Письмо успешно отправлено!", response);
+      alert("Ваше сообщение успешно отправлено!");
+    }, function (error) {
+      console.error("Ошибка отправки письма:", error);
+      alert("Произошла ошибка при отправке сообщения. Пожалуйста, попробуйте еще раз.");
+    });
+  }
+  else{
+    alert("Пожалуйста заполните поля");
+  }
+}
 
 
 
@@ -57,9 +71,9 @@ function switchVisibilite(el) {
 
 function scrollContainer(containerId, direction) {
   const container = document.getElementById(containerId);
-  const scrollAmount = container.clientWidth
-
+  const scrollAmount = container.clientWidth;
   if (direction === 'prev') {
+
     container.scrollBy({
       left: -scrollAmount,
       behavior: 'smooth'
@@ -70,16 +84,15 @@ function scrollContainer(containerId, direction) {
       behavior: 'smooth'
     });
   }
+
   setTimeout(() => {
     buttonCheck(container);
-  }, 500);
-
+  }, 600);
 };
 
 function buttonCheck(container) {
   const prevButton = container.previousElementSibling;
   const nextButton = container.nextElementSibling;
-
   if (container.scrollLeft <= 0) {
     prevButton.style.display = 'none'; // Скрываем кнопку "назад"
   } else {
@@ -95,75 +108,66 @@ function buttonCheck(container) {
 
 
 const apiKey = "AIzaSyD6BNAufMQra7YPJH6HdaTnkKLbothsoG8";
-const playlistId = "PLgToFcDSRCvCZL4SSiVtUXapo1TK2cowZ";
-const chanelId = "UCg4ahWqLsi7mgYyIIAW7oLQ";
+const chanelId = "UCK1Z9PQXRZO-n5Fv7LEHPLw";
 
-async function fetchVideos(pageToken = '') {
-    const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&part=snippet&maxResults=50&playlistId=PLgToFcDSRCvCZL4SSiVtUXapo1TK2cowZ&key=${apiKey}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-    return data;
+async function fetchVideos(playlistId) {
+  const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&part=snippet&maxResults=50&playlistId=${playlistId}&key=${apiKey}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
 
-async function loadVideos() {
-    let nextPageToken = '';
-    const videoContainer = document.getElementById('video-container');
+async function loadVideos(containerId, playlistId) {
+  let nextPageToken = '';
+  const videoContainer = document.getElementById(containerId);
 
-    do {
-        const data = await fetchVideos(nextPageToken);
-        data.items.forEach(item => {
-            const videoId = item.contentDetails.videoId;
+  do {
+    const data = await fetchVideos(playlistId);
+    data.items.forEach(item => {
+      const videoId = item.contentDetails.videoId;
 
-            const videoDiv = document.createElement('div');
-            videoDiv.classList.add('video');
+      const videoDiv = document.createElement('div');
+      videoDiv.classList.add('video');
 
-            const iframe = document.createElement('iframe');
-            iframe.width = "325";
-            iframe.height = "215";
-            iframe.src = `https://www.youtube.com/embed/${videoId}`;
-            iframe.frameBorder = "0";
-            iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-            iframe.allowFullscreen = true;
-            videoDiv.appendChild(iframe);
+      const iframe = document.createElement('iframe');
+      iframe.width = "325";
+      iframe.height = "215";
+      iframe.src = `https://www.youtube.com/embed/${videoId}`;
+      iframe.frameBorder = "0";
+      iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+      iframe.allowFullscreen = true;
+      videoDiv.appendChild(iframe);
 
-            videoContainer.appendChild(videoDiv);
+      videoContainer.appendChild(videoDiv);
 
-            videoDiv.addEventListener('click', () => {
-                showOverlay(videoId);
-            });
-        });
-        nextPageToken = data.nextPageToken;
-    } while (nextPageToken);
+      videoDiv.addEventListener('click', () => {
+        showOverlay(videoId);
+      });
+    });
+    nextPageToken = data.nextPageToken;
+  } while (nextPageToken);
 }
 
 function showOverlay(videoId) {
-    let overlay = document.querySelector('.overlay');
-    if (!overlay) {
-        overlay = document.createElement('div');
-        overlay.classList.add('overlay');
-        document.body.appendChild(overlay);
+  let overlay = document.querySelector('.overlay');
+  if (!overlay) {
+    overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+    document.body.appendChild(overlay);
 
-        overlay.addEventListener('click', () => {
-            overlay.classList.remove('active');
-        });
-    }
-
-    overlay.innerHTML = `<iframe src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-    overlay.classList.add('active');
-}
-
-loadVideos();
-
-
-
-function scrollContainer(containerId, direction) {
-  const container = document.getElementById(containerId);
-  const scrollAmount = 300; // Установите количество пикселей для прокрутки
-
-  if (direction === 'prev') {
-    container.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
-  } else if (direction === 'next') {
-    container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    overlay.addEventListener('click', () => {
+      overlay.classList.remove('active');
+    });
   }
+
+  overlay.innerHTML = `<iframe src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  overlay.classList.add('active');
 }
+
+function ScrollToReg(){
+  
+  document.querySelector('#reg').scrollIntoView({behavior:'smooth'});
+}
+
+
+
