3903a072f1be9ae2eb420befb7119adfa312d42f
lNektosl <105857065+lNektosl@users.noreply.github.com>
2024-07-01 20:00:03 +0300

    env netlify

--- netlify/functions/getEnvVariables.js
+++ netlify/functions/getEnvVariables.js
@@ -1,12 +1,12 @@
 exports.handler = async function (event, context) {
-    const youtubeApiKey = process.env.YOUTUBE_APIKEY;
-    const jsemailApiKey = process.env.JSEMAIL_APIKEY;
-  
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        YOUTUBE_APIKEY: youtubeApiKey,
-        JSEMAIL_APIKEY: jsemailApiKey,
-      }),
-    };
+  const youtubeApiKey = process.env.YOUTUBE_APIKEY;
+  const jsemailApiKey = process.env.JSEMAIL_APIKEY;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      YOUTUBE_APIKEY: youtubeApiKey,
+      JSEMAIL_APIKEY: jsemailApiKey,
+    }),
   };
+};
